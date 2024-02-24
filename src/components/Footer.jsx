872830ca470e132d98f787f5bf7b5baaf23e1f3c
Rachana uniyal <rachanauniyal@Rachanas-Air.domain.name>
2024-02-24 15:25:03 +0530

    added github code link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <div className="fixed inset-x-0 bottom-0 flex items-center bg-gray-700 justify-center p-2 mt-28 md:p-4 h-16 md:h-18">
-            <a href="" className="text-xs md:text-sm lg:text-base text-gray-50">Github</a>
+            <a href="https://github.com/rachana-uniyal/weatherly" className="text-xs md:text-sm lg:text-base text-gray-50">Github</a>
             <a href="https://openweathermap.org/api" className="text-xs md:text-sm lg:text-base text-gray-50 ml-4 md:ml-10">OpenWeather</a>
         </div>
     );
